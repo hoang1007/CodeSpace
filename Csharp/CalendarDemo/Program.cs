6f2a4cca37410928289334b4349d28ed597862c3
hoang1007 <20020414@vnu.edu.vn>
2021-06-05 15:39:11 +0700

    add Calendar

---
+++ Csharp/CalendarDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalendarDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap thang, nam (MM/YYYY): ");
+            string[] tokens = Console.ReadLine().Split();
+
+            int month = int.Parse(tokens[0]),
+                year = int.Parse(tokens[1]);
+            
+            Calendar calendar = new Calendar(month, year);
+            calendar.callCalendar();
+        }
+    }
+}
